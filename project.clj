3aa9cc2a5f1c55588a92e47bd89b7786d6773fa6
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-06-20 14:30:15 +0200

    v0.6.1: updated deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject matthiasn/systems-toolbox-zipkin "0.6.1-SNAPSHOT"
+(defproject matthiasn/systems-toolbox-zipkin "0.6.1"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/matthiasn/systems-toolbox-zipkin"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.zipkin.brave/brave "4.3.3"]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[io.zipkin.brave/brave "4.4.0"]
                  [io.zipkin.reporter/zipkin-reporter "0.10.0"]
                  [io.zipkin.reporter/zipkin-sender-okhttp3 "0.10.0"]]
-  :profiles {:dev {:dependencies   [[org.clojure/clojure "1.9.0-alpha17"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha17"]]}})
